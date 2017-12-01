18d7fd0ac5a111288e3a03ef09e097dd0fc4c9d6
Adam H. Sparks <adamhsparks@gmail.com>
2017-12-01 22:07:09 +1000

    Fix .validate_dsn function

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -2,15 +2,14 @@
 .validate_dsn <- function(dsn) {
   if (is.null(dsn)) {
     dsn <- getwd()
+  } else  if (substr(dsn, nchar(dsn) - 1, nchar(dsn)) == "//") {
+    dsn <- substr(dsn, 1, nchar(dsn) - 2)
+  } else if (substr(dsn, nchar(dsn), nchar(dsn)) == "/" |
+             substr(dsn, nchar(dsn), nchar(dsn)) == "\\") {
+    dsn <- substr(dsn, 1, nchar(dsn) - 1)
+  } else if (!dir.exists(dsn) & !dir.exists(dsn)) {
+    stop("\nThis dsn does not exist: ", dsn, ".\n")
   } else {
-    if (substr(dsn, nchar(dsn) - 1, nchar(dsn)) == "//") {
-      dsn <- substr(dsn, 1, nchar(dsn) - 2)
-    } else if (substr(dsn, nchar(dsn), nchar(dsn)) == "/" |
-               substr(dsn, nchar(dsn), nchar(dsn)) == "\\") {
-      dsn <- substr(dsn, 1, nchar(dsn) - 1)
-    }
-    if (!dir.exists(dsn) & !dir.exists(dsn)) {
-      stop("\nThis dsn does not exist: ", dsn, ".\n")
-    }
+    dsn <- dsn
   }
 }
