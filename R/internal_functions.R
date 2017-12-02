471acff883ce79c99a756e61b9c7003707118ed1
Adam H. Sparks <adamhsparks@gmail.com>
2017-12-03 09:42:31 +1000

    Clean up interpolate_gsod
    Function can now take a list of data frames or list of CSV files.
    
    Update documentation
    
    Remove "cores" as it's not internal, can use `mclapply` outside of this function.

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -1,6 +1,46 @@
 `%notin%` <- Negate("%in%")
 
 #' @noRd
+.check_gsod <- function(gsod) {
+  if (is.null(gsod)) {
+    stop("You must supply a list of GSOD data files for interpolation")
+  } else if (typeof(gsod[[1]]) == "character") {
+    gsod <- gsod
+  } else {
+    # import the file for interpolation
+    gsod <- readr::read_csv(gsod, col_types = "cdddcddddd")
+  }
+
+}
+
+#' @noRd
+.check_vars <- function(vars) {
+  if (is.null(vars)) {
+    vars <- "TEMP"
+  } else {
+    vars <- toupper(vars)
+
+    if (any(vars %notin% c("TEMP", "RH", "MAX", "MIN"))) {
+      stop("One or more of your weather variable(s) are\n",
+           "not valid for interpolation.\n")
+    }
+  }
+}
+
+#' @noRd
+# check year list, if not specified default to current year
+.check_year <- function(year_list) {
+  if (is.null(year_list)) {
+    message(
+      "\nYou have not specified any years to fetch, defaulting to ",
+      format(Sys.Date(), "%Y"),
+      ".\n"
+    )
+    year_list <- format(Sys.Date(), "%Y")
+  }
+}
+
+#' @noRd
 .validate_dsn <- function(dsn) {
   if (is.null(dsn)) {
     dsn <- path.expand("~")
@@ -27,64 +67,6 @@
     stop("\nThis dsn does not exist: ", dsn, ".\n")
   } else {
     dsn <- dsn
-  }
-}
-
-#' @noRd
-# check year list, if not specified default to current year
-.check_year <- function(year_list) {
-  if (is.null(year_list)) {
-    message(
-      "\nYou have not specified any years to fetch, defaulting to ",
-      format(Sys.Date(), "%Y"),
-      ".\n"
-    )
-    year_list <- format(Sys.Date(), "%Y")
-  }
-}
-
-#' @noRd
-.check_vars <- function(vars) {
-  if (is.null(vars)) {
-    vars <- "TEMP"
-  } else {
-    vars <- toupper(vars)
-
-    if (any(vars %notin% c("TEMP", "RH", "MAX", "MIN"))) {
-      stop("One or more of your weather variable(s) are\n",
-           "not valid for interpolation.\n")
-    }
-  }
-}
-
-#' @noRd
-.check_bz2 <- function(bz2_file) {
-  if (is.null(bz2_file)) {
-    stop("You must supply a list of GSOD data files for interpolation")
-  }
-}
-
-#' @noRd
-# check OS and set cores to 1 if NULL, Windows OS or unknown
-# make sure that number of cores specifed less than number available
-.validate_cores <- function(cores) {
-  if (is.null(cores)) {
-    cores <- 1
-  }
-  if (.Platform$OS.type == "windows") {
-    cores <- 1
-  } else if (Sys.info()["sysname"] == "Darwin") {
-    if (cores <= parallel::detectCores()) {
-      cores <- cores
-    } else {
-      stop("You specified more cores than available for processing.")
-    }
-  } else if (.Platform$OS.type == "unix") {
-    if (cores <= parallel::detectCores()) {
-      cores <- cores
-    } else {
-      stop("You specified more cores than available for processing.")
-    }
   }
 }
 
