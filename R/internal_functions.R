626daa2061db9cbe723886471e4aeb93cb250378
Adam H. Sparks <adamhsparks@gmail.com>
2017-12-02 12:56:38 +1000

    Fix bug where dsn not set to user directory if unspecified

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -1,7 +1,7 @@
 #' @noRd
 .validate_dsn <- function(dsn) {
   if (is.null(dsn)) {
-    dsn <- getwd()
+    dsn <- path.expand("~")
   } else  if (substr(dsn, nchar(dsn) - 1, nchar(dsn)) == "//") {
     dsn <- substr(dsn, 1, nchar(dsn) - 2)
   } else if (substr(dsn, nchar(dsn), nchar(dsn)) == "/" |
@@ -16,3 +16,16 @@
 
 
 `%notin%` <- Negate("%in%")
+
+#' @noRd
+# check year list, if not specified default to current year
+.check_year <- function(year_list) {
+  if (is.null(year_list)) {
+    message(
+      "\nYou have not specified any years to fetch, defaulting to ",
+      format(Sys.Date(), "%Y"),
+      ".\n"
+    )
+    year_list <- format(Sys.Date(), "%Y")
+  }
+}
