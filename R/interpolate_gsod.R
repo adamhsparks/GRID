0379f05d3f3cc2245369d4a68eb1aca5d12555b0
Adam H. Sparks <adamhsparks@gmail.com>
2017-12-11 10:09:49 +1000

    Take only the file names from the end, so directory doesn't matter when assigning names in list

--- R/interpolate_gsod.R
+++ R/interpolate_gsod.R
@@ -109,7 +109,9 @@ interpolate_GSOD <- function(file_list = NULL,
   out <- list(c(TEMP = TEMP, MAX = MAX, MIN = MIN, RH = RH))
 
   # assign GSOD_YYYY to list objects before returning list
-  names(out) <- paste0(substr(file_list, 1, 9))
+  names(out) <- paste0(substr(file_list,
+                              nchar(file_list) - 9 + 1,
+                              nchar(file_list)))
 
   return(out)
 }
