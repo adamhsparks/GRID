dfb0514375258dfb746e6bff61b69296f152d109
Adam H. Sparks <adamhsparks@gmail.com>
2018-10-07 09:02:57 +1000

    Updates documentation for make_dem()

--- R/make_DEM.R
+++ R/make_DEM.R
@@ -1,26 +1,24 @@
 
-#' Get and Aggregate a Digital Elevation Model for Use in Interpolating GSOD Data
+#' Get and Aggregate a Digital Elevation Model for Use in Interpolating \acronym{GSOD} Data
 #'
 #' Fetches a digital elevation model (DEM) from WorldClim data, crops and
 #' aggregates to a larger spatial resolution and crops at -60/60 degrees
 #' latitude for use with other weather data that provide rainfall, \emph{e.g.}
-#' NASA - POWER at 0.5 arc degrees or TRMM at 0.25 arc degrees. Mainly for use in
-#' crop modelling exercises.
+#' \acoronym{NASA} \acronym{POWER} at 0.5 arc degrees or \acronym{TRMM} at 0.25
+#' arc degrees. Mainly for use in crop modelling exercises.
 #'
 #' @param resolution Resolution to aggregate the digital elevation model to in
 #' arc-degrees, e.g. 1 = 1 arc degree, 0.5 = one half arc degree, 0.25 = one
 #' quarter arc degree. Valid options are `1`, `0.5` and `0.25`.
-#' @param dsn Optional. Directory where resulting DEM file is to be saved. If
-#' unspecified a spatial object is returned in the R session. If a DEM exists,
-#' it will be overwritten with the new one of the same resolution. If a new
-#' resolution is specified, a new file will be created.
+#' @param dsn Optional. Directory where resulting \acronym{DEM} file is to be
+#' saved. If unspecified a spatial object is returned in the R session. If a
+#' \acronym{DEM} exists, it will be overwritten with the new one of the same
+#' resolution. If a new resolution is specified, a new file will be created.
 #'
 #' @return A `[raster::raster()]` object of a digital elevation model cropped to
 #' -60/60 degrees latitude and aggregated by the requested factor.
 #'
 #' @author Adam H. Sparks, \email{adamhsparks@@gmail.com}
-#'
-#' @export
 #'
 #' @examples
 #'
@@ -30,7 +28,8 @@
 #'
 #' DEM <- make_DEM(dsn = "~/Data/DEM")
 #' }
-#'
+#' @export make_DEM
+
 make_DEM <- function(resolution = NULL, dsn = NULL) {
 
   dsn <- .validate_dsn(dsn)
