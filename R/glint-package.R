b9ac584fedce7b906d4b1d082c87858717c66a4e
Adam H. Sparks <adamhsparks@gmail.com>
2018-10-07 08:58:55 +1000

    Fixes glint.Rd file

--- R/glint-package.R
+++ R/glint-package.R
@@ -3,4 +3,7 @@
 #' Interpolates 'GSOD' temperature data as provided by the '[GSODR::GSODR]'
 #' package.
 #'
+#' @name glint
 #' @author Adam H. Sparks, \email{adamhsparks@@gmail.com}
+#' @docType package
+NULL
