2b0f0b34f9e1458302f622f2e95c0ccbe317db11
Adam Sparks <adamhsparks@gmail.com>
2018-11-20 12:03:00 +0000

    Update documentation formatting

--- R/glint-package.R
+++ R/glint-package.R
@@ -1,7 +1,7 @@
-#' Global ('GSOD') Interpolated Temperatures
+#' Global ('GSOD') Interpolated Temperatures and Relative Humidity
 #'
-#' Interpolates 'GSOD' temperature data as provided by the '[GSODR::GSODR]'
-#' package.
+#' Interpolates 'GSOD' temperature and relative humidity data as provided by the
+#' \pkg{GSODR} package.
 #'
 #' @name glint
 #' @author Adam H. Sparks, \email{adamhsparks@@gmail.com}
