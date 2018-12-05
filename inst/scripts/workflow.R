ae27b270833be24efca32325b80d80c531a1f899
Adam H. Sparks <adamhsparks@gmail.com>
2018-12-05 11:26:38 +0000

    Add new script file for running as a batch command, R CMD BATCH

---
+++ inst/scripts/workflow.R
@@ -0,0 +1,14 @@
+
+# set up session
+library(glint)
+future::plan("multisession")
+years <- as.list(seq(from = 1983, to = 2015, by = 1))
+ifelse(!dir.exists(file.path("/mnt", "GSOD")), 
+       dir.create(file.path("/mnt", "GSOD")),
+       FALSE)
+
+# fetch GSOD data and store in "/mnt/GSOD" as `fst` files
+lapply(X = years, FUN = make_GSOD_set, dsn = "/mnt/GSOD")
+
+# create DEM files
+DEM <- make_DEM(dsn = "/mnt/DEM")
